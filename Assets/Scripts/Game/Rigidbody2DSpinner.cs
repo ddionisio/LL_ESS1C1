a4e042b2c16c0e43c0f667c0ec5b04789ccd0b2b
ddionisio <garapata@gmail.com>
2018-05-14 20:38:36 -0400

    level 3

---
+++ Assets/Scripts/Game/Rigidbody2DSpinner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//make sure body is kinematic
+public class Rigidbody2DSpinner : MonoBehaviour {
+    public float rotatePerSecond;
+
+    private Rigidbody2D mBody;
+
+    void Awake() {
+        mBody = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate () {
+        float deltaAngle = rotatePerSecond * Time.fixedDeltaTime;
+
+        mBody.MoveRotation(mBody.rotation + deltaAngle);
+	}
+}
---
+++ Assets/Scripts/Game/TriggerGOActiveSwitch.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerGOActiveSwitch : MonoBehaviour {
+    public GameObject activeGO;
+    public GameObject inactiveGO;
+
+    void Awake() {
+        //if(activeGO) activeGO.SetActive(true);
+        if(inactiveGO) inactiveGO.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision) {
+        if(activeGO) activeGO.SetActive(!activeGO.activeSelf);
+        if(inactiveGO) inactiveGO.SetActive(!inactiveGO.activeSelf);
+    }
+}
