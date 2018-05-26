3a235a6286766980484c74e93936ee7a6b81b1b7
ddionisio <garapata@gmail.com>
2018-05-25 22:05:02 -0400

    level 1 art stuff and a bunch of other stuff

---
+++ Assets/Scripts/Editor/ProBuilderObjectOffset.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+using ProBuilder.Core;
+
+public class ProBuilderObjectOffset {
+
+    [MenuItem("Game/ProBuilder/Object/Apply Unity Position")]
+    static void ObjectApplyUnityPosition() {
+        var gos = Selection.gameObjects;
+
+        for(int i = 0; i < gos.Length; i++) {
+            pb_Object obj = gos[i].GetComponent<pb_Object>();
+            if(obj) {
+                var pos = gos[i].transform.localPosition;
+
+                Undo.RecordObject(gos[i].transform, "Reset Local Position");
+                gos[i].transform.localPosition = Vector3.zero;
+
+                Undo.RecordObject(obj, "Apply Vertices");
+                                
+                var vtx = obj.vertices;
+                for(int vI = 0; vI < vtx.Length; vI++) {
+                    vtx[vI] += pos;
+                }
+
+                obj.SetVertices(vtx);
+                obj.Refresh();
+            }
+        }
+    }
+}
