e2d76c4d59aafba0cf8f66849e849354468c59d0
ddionisio <garapata@gmail.com>
2018-05-31 00:15:06 -0400

    ending stuff

--- Assets/Scripts/Game/EndingController.cs
+++ Assets/Scripts/Game/EndingController.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingController : MonoBehaviour {
+    public M8.Animator.AnimatorData animator;
+    public string take = "play";
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Text scoreLabel;
+
+    void Awake() {
+        //initialize hud
+        HUD.instance.isGameActive = false;
+    }
+
+    IEnumerator Start() {
+        //wait for scene transition
+        while(M8.SceneManager.instance.isLoading)
+            yield return null;
+
+        //setup infos
+        if(LoLManager.isInstantiated) {
+            if(scoreLabel) scoreLabel.text = LoLManager.instance.curScore.ToString();
+        }
+
+        //play animation
+        if(animator && !string.IsNullOrEmpty(take))
+            animator.Play(take);
+    }
 }
